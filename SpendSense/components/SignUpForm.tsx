2302a78a16f0a82484038e9e2e195865c28d6b19
Ben926 <benjamin.koh.ts@gmail.com>
2024-05-20 13:18:56 +0800

    added signup

--- SpendSense/components/SignUpForm.tsx
+++ SpendSense/components/SignUpForm.tsx
@@ -1,23 +1,26 @@
 import {TextInput, View, Button} from "react-native"
 import { useState } from "react";
 
-interface LogInFormProps {
+interface SignUpFormProps {
     login: React.ReactNode;
     signup: React.ReactNode;
   }
 
-  const LogInForm: React.FC<LogInFormProps> = ({login, signup}) => {
+const SignUpForm: React.FC<SignUpFormProps> = ({login, signup}) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     return (<View>
     <TextInput textAlign = "center" placeholderTextColor = "black" placeholder= "Username" 
     value = {username} onChangeText={setUsername} style ={{color: "black", borderColor: "gray"}}></TextInput>
     <TextInput textAlign = "center" placeholderTextColor = "black" placeholder = "Password" 
     value = {password} onChangeText={setPassword} style ={{color: "black", borderColor: "gray"}}></TextInput>
+    <TextInput textAlign = "center" placeholderTextColor = "black" placeholder = "Confirm Password" 
+    value = {confirmPassword} onChangeText={setConfirmPassword} style ={{color: "black", borderColor: "gray"}}></TextInput>
     {login}
     {signup}
     </View>
     );
 }
 
-export default LogInForm;
+export default SignUpForm;
