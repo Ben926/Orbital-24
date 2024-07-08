df1486a24b7ea5a31af0d3520f36e24c537fa049
Ben926 <benjamin.koh.ts@gmail.com>
2024-07-09 02:14:15 +0800

    added icons to nav bar

--- SpendSense/app/(pages)/_layout.tsx
+++ SpendSense/app/(pages)/_layout.tsx
@@ -1,9 +1,16 @@
 import { Tabs } from "expo-router";
+import { Feather, Entypo } from '@expo/vector-icons';
 
 export default function Layout() {
     return <Tabs>
-        <Tabs.Screen name = "Home" options = {{title: "Home", headerShown: false}}></Tabs.Screen>
-        <Tabs.Screen name = "BudgetPage" options = {{title: "Budgets", headerShown: false}}></Tabs.Screen>
-        <Tabs.Screen name = "GoalPage" options = {{title: "Goals", headerShown: false}}></Tabs.Screen>
+        <Tabs.Screen name = "Home" options = {{title: "Home", headerShown: false, tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" size={24} color="black" />
+          )}}></Tabs.Screen>
+        <Tabs.Screen name = "BudgetPage" options = {{title: "Budgets", headerShown: false, tabBarIcon: ({ color, size }) => (
+            <Feather name="dollar-sign" size={24} color="black" />
+          )} }></Tabs.Screen>
+        <Tabs.Screen name = "GoalPage" options = {{title: "Goals", headerShown: false, tabBarIcon: ({ color, size }) => (
+            <Feather name="target" size={24} color="black" />
+          )}}></Tabs.Screen>
     </Tabs>
 }
